6ad6dcdc0379e17ce2e3c3a3d98947806ee4837b
Mohamed Lamrani <mlamrani@c3r6p10.1337.ma>
2024-03-03 11:06:52 +0100

    s1m0x00

--- fractol/test.c
+++
@@ -1,9 +0,0 @@
-	else if (*str == '-' || *str == '+')
-		{
-            sign_count++;
-            if (sign_count > 1 || !numeric_flag) 
-			{
-                fd_putstr(ERROR_MESSAGE_1, STDERR_FILENO);
-                exit(1);
-            }
-        } 
